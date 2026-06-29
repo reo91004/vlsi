634474d7c391ed0ecee290ef0f09b4efed2659af
reo91004 <reo91004@gmail.com>
2026-06-29 14:35:36 +0900

    fix

--- TB_4bit_FA.v
+++ TB_4bit_FA.v
@@ -1,7 +1,9 @@
 module TB_4bit_FA;
 
-    reg [3:0] x, y;
+    reg [3:0] x;
+    reg [3:0] y;
     reg c_in;
+
     wire [3:0] sum;
     wire c_out;
 
@@ -14,25 +16,41 @@ module TB_4bit_FA;
     );
 
     initial begin
-        $dumpfile("FA4_dump.vcd");
-        $dumpvars(0, TB_4bit_FA);
+        $timeformat(-9, 0, " ns", 10);
     end
 
     initial begin
-        $display("time\tx\ty\tc_in\tc_out sum");
+        x = 4'b0000;
+        y = 4'b0000;
+        c_in = 1'b0;
 
-        x = 4'b0000; y = 4'b0000; c_in = 0; #50;
-        x = 4'b0011; y = 4'b0101; c_in = 0; #50;  // 3 + 5 = 8
-        x = 4'b0111; y = 4'b0001; c_in = 0; #50;  // 7 + 1 = 8
-        x = 4'b1111; y = 4'b0001; c_in = 0; #50;  // 15 + 1 = 16
-        x = 4'b1010; y = 4'b0101; c_in = 1; #50;  // 10 + 5 + 1 = 16
+        #50;
+        x = 4'b0101;
+        y = 4'b0001;
+        c_in = 1'b0;
 
+        #50;
+        x = 4'b1111;
+        y = 4'b0001;
+        c_in = 1'b0;
+
+        #100;
+        x = 4'b0110;
+        y = 4'b0001;
+        c_in = 1'b1;
+
+        #50;
         $finish;
     end
 
-    always @(x or y or c_in or sum or c_out) begin
-        $display("%0t\t%b\t%b\t%b\t%b_%b",
-                 $time, x, y, c_in, c_out, sum);
+    initial begin
+        $dumpfile("FA_4B_dump.vcd");
+        $dumpvars(0, TB_4B_FA);
+    end
+
+    always @(sum or c_out) begin
+        $display("time = %0t \t input value: x = %b, y = %b, c_in = %b \t output value: sum = %b, c_out = %b",
+                 $realtime, x, y, c_in, sum, c_out);
     end
 
 endmodule
